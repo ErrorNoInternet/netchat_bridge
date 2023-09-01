2a49e676165e2ecbec51bd61c3bc2e8e1561fb71
ErrorNoInternet <errornointernet@envs.net>
2023-09-01 12:19:31 +0800

    Fix requests by adding user agent

--- src/netchat.rs
+++ src/netchat.rs
@@ -1,17 +1,38 @@
-const NETCHAT_INSTANCE: &str = "https://netchat.repl.co/";
+const NETCHAT_INSTANCE: &str = "https://netchat.repl.co";
 
-pub async fn get_room(name: String, password: String) -> Result<String, String> {
-    match reqwest::get(format!("{NETCHAT_INSTANCE}/{password}/{name}/allMessages")).await {
-        Ok(response) => match response.text().await {
-            Ok(text) => Ok(text),
-            Err(error) => Err(format!("failed to get response text: {error}")),
-        },
+async fn make_request(url: &str) -> Result<reqwest::Response, reqwest::Error> {
+    let client = reqwest::ClientBuilder::new()
+        .user_agent(format!(
+            "netchat_bridge/{} (url:{url}) reqwest",
+            env!("CARGO_PKG_VERSION")
+        ))
+        .build()
+        .unwrap();
+    client.get(url).send().await
+}
+
+pub async fn get_room(name: &str, password: &str) -> Result<String, String> {
+    match make_request(&format!("{NETCHAT_INSTANCE}/{password}/{name}/allMessages")).await {
+        Ok(response) => {
+            if response.status().is_server_error() {
+                Err(format!("encountered server error"))
+            } else {
+                match response.text().await {
+                    Ok(text) => Ok(text),
+                    Err(error) => Err(format!("failed to get response text: {error}")),
+                }
+            }
+        }
         Err(error) => Err(format!("failed to send GET request: {error}")),
     }
 }
 
-pub async fn is_initializing(name: String, password: String) -> Result<bool, String> {
+pub async fn is_initializing(name: &str, password: &str) -> Result<bool, String> {
     Ok(get_room(name, password)
         .await?
         .contains("<title>Initializing...</title><style>body"))
+}
+
+pub async fn is_correct_password(name: &str, password: &str) -> Result<bool, String> {
+    Ok(get_room(name, password).await? != "Wrong Password!")
 }
