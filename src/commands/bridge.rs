0354410bf756aee65e3ed1d832483d8ccdba579a
ErrorNoInternet <errornointernet@envs.net>
2023-09-01 12:18:40 +0800

    Add more checks

--- src/commands/bridge.rs
+++ src/commands/bridge.rs
@@ -1,5 +1,5 @@
 use super::CommandInput;
-use crate::{language::get_text, netchat, permissions::Action, utilities};
+use crate::{language::get_text, logging::log_error, netchat, permissions::Action, utilities};
 
 pub async fn bridge_command(command_input: &CommandInput) {
     if utilities::handle_permissions(command_input, Action::BridgeCreate).await {
@@ -19,25 +19,77 @@ pub async fn bridge_command(command_input: &CommandInput) {
     }
     let room_name = &command_input.arguments[0];
     let room_password = &command_input.arguments[1];
-    utilities::set_typing(&command_input.room, true).await;
 
-    let is_initializing =
-        match netchat::is_initializing(room_name.to_string(), room_password.to_string()).await {
-            Ok(is_initializing) => is_initializing,
-            Err(error) => {
-                utilities::send_html_message(
+    match command_input
+        .matrix_state
+        .database
+        .get(&format!("bridge.{}", command_input.room.room_id().as_str()))
+    {
+        Ok(value) => match value {
+            Some(_value) => {
+                utilities::send_plain_message(
                     &command_input.room,
-                    &get_text("fetch_room_failed").replace("{error}", &error.to_string()),
+                    get_text("room_already_bridged"),
                 )
                 .await;
                 return;
             }
-        };
-    if is_initializing {
-        utilities::send_plain_message(&command_input.room, get_text("room_currently_initializing"))
+            None => (),
+        },
+        Err(error) => {
+            log_error(&error);
+            utilities::send_plain_message(
+                &command_input.room,
+                get_text("database_error")
+                    .replace("{error}", &error)
+                    .as_str(),
+            )
             .await;
-        return;
+            return;
+        }
     }
 
-    //    utilities::send_plain_message(&command_input.room, "Success!").await;
+    utilities::set_typing(&command_input.room, true).await;
+    match netchat::is_initializing(room_name, room_password).await {
+        Ok(is_initializing) => {
+            if is_initializing {
+                utilities::send_plain_message(
+                    &command_input.room,
+                    get_text("room_currently_initializing"),
+                )
+                .await;
+                return;
+            }
+        }
+        Err(error) => {
+            log_error(&error);
+            utilities::send_html_message(
+                &command_input.room,
+                &get_text("fetch_room_failed").replace("{error}", &error.to_string()),
+            )
+            .await;
+            return;
+        }
+    };
+
+    match netchat::is_correct_password(room_name, room_password).await {
+        Ok(is_correct_password) => {
+            if !is_correct_password {
+                utilities::send_plain_message(&command_input.room, get_text("room_wrong_password"))
+                    .await;
+                return;
+            }
+        }
+        Err(error) => {
+            log_error(&error);
+            utilities::send_html_message(
+                &command_input.room,
+                &get_text("fetch_room_failed").replace("{error}", &error.to_string()),
+            )
+            .await;
+            return;
+        }
+    };
+
+    //    utilities::send_plain_message(&command_input.room, "all checks passed!").await;
 }
