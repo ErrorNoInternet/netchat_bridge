02d49c20b20613ca8d9cde784d09f3be751abfb7
ErrorNoInternet <errornointernet@envs.net>
2023-09-01 12:18:50 +0800

    Add more translations

--- src/language.rs
+++ src/language.rs
@@ -6,8 +6,11 @@ static TEXTS: phf::Map<&'static str, &'static str> = phf_map! {
     "command_no_permissions" => "You do not have the permissions to use this command! This command requires power level <code>{minimum_power_level}</code>.",
     "fetch_permissions_failed" => "Uh oh! An error occurred while fetching your room permissions (<code>{error}</code>). For safety reasons, you have been denied access to use this command.",
     "missing_arguments" => "You did not supply enough arguments! The command requires at least {count} arguments ({arguments}).",
-    "fetch_room_failed" => "Uh oh! An error occurred while fetching data about that NetChat room (<code>{error}</code>). Please try again later.",
-    "room_currently_initializing" => "Seems like that NetChat room is currently being initialized. Please try again in a few minutes."
+    "database_error" => "Uh oh! Something went wrong while querying the database (<code>{error}</code>). Please try again later.",
+    "fetch_room_failed" => "Uh oh! An error occurred while fetching that NetChat room (<code>{error}</code>). Please try again later.",
+    "room_currently_initializing" => "Seems like that NetChat room is currently being initialized. Please try again in a few minutes.",
+    "room_already_bridged" => "Hmm, seems like this room has already been bridged. You can use the unbridge command to unbridge this room and try again.",
+    "room_wrong_password" => "Hmm, I can't seem to access that room, maybe the password you supplied is wrong?"
 };
 
 pub fn get_text(key: &str) -> &str {
